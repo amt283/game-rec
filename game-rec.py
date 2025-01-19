04bb4855d8713f75c6f9a7daf65ac5d8b74c6887
amt283 <athompson283@mycr.redwoods.edu>
2025-01-19 09:50:57 +0000

    .py file created with pseudocode

---
+++ game-rec.py
@@ -0,0 +1,13 @@
+# Asks for input on type of game looking for (liked, never played, unfinished, missing achievements)
+# Asks for game length (??)
+# Searches dict of games (two dicts? One for type, second for length?)
+# (maybe asks how many games to suggest?)
+# Outputs suggestion 
+# Have the ability to refresh list based on previous choices
+# Have ability to go back to previous steps and change settings
+# Have ability to go back to main menu and start over at any time
+
+def main():
+    ...
+
+main()
